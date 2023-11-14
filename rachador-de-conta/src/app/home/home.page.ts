91df0ef2c6262d24bf79339175b595a86d990a56
Daniel Rovetta <daniel.rovettapassos@gmail.com>
2023-11-14 16:29:38 -0300

    Melhorando o estilo das paginas

--- rachador-de-conta/src/app/home/home.page.ts
+++ rachador-de-conta/src/app/home/home.page.ts
@@ -20,39 +20,6 @@ export class HomePage {
   valorTotalPessoa = 0;
 
   constructor(private business: BusinessService) {
-    this.business.insertPessoa(new Pessoa(1, "Natalia"));
-    this.business.insertPessoa(new Pessoa(2, "Daniel"));
-    this.business.insertPessoa(new Pessoa(3, "Lucas"));
-    this.business.insertPessoa(new Pessoa(4, "Gabriela"));
-
-
-    this.business.insertItem(new Item(1, "Linguiça", 17.41));
-    this.business.insertItem(new Item(2, "Refrigerante", 14.98));
-    this.business.insertItem(new Item(3, "Energético", 15.99));
-    this.business.insertItem(new Item(4, "Vodka", 22.99));
-    this.business.insertItem(new Item(5, "Carne", 59.27));
-    this.business.insertItem(new Item(6, "Suco", 10.00));
-
-
-    this.business.insertConsumo(new Consumo(0, new Pessoa(1, "Natalia"), new Item(1, "Linguiça", 17.41), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(1, "Natalia"), new Item(2, "Refrigerante", 14.98), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(1, "Natalia"), new Item(3, "Energético", 15.99), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(1, "Natalia"), new Item(4, "Vodka", 22.99), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(1, "Natalia"), new Item(5, "Carne", 59.27), 1));
-
-    this.business.insertConsumo(new Consumo(0, new Pessoa(2, "Daniel"), new Item(1, "Linguiça", 17.41), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(2, "Daniel"), new Item(2, "Refrigerante", 14.98), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(2, "Daniel"), new Item(3, "Energético", 15.99), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(2, "Daniel"), new Item(4, "Vodka", 22.991), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(2, "Daniel"), new Item(5, "Carne", 59.27), 1));
-
-    this.business.insertConsumo(new Consumo(0, new Pessoa(3, "Lucas"), new Item(3, "Energético", 15.99), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(3, "Lucas"), new Item(4, "Vodka", 22.991), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(3, "Lucas"), new Item(5, "Carne", 59.27), 2));
-
-    this.business.insertConsumo(new Consumo(0, new Pessoa(4, "Gabriela"), new Item(1, "Linguiça", 17.41), 1));
-    this.business.insertConsumo(new Consumo(0, new Pessoa(4, "Gabriela"), new Item(6, "Suco", 10.00), 1));
-
   }
 
   getAllConsumoByIdPessoa(id: number) {
